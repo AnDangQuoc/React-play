46137813bd47828cb3a194bedcfc6bbbd379e474
Dang Quoc An <dangquocan95@gmail.com>
2018-07-15 17:09:24 +0700

    Add Container

--- ant_play/src/App.js
+++ ant_play/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Container from './container/Container';
 import { Button } from 'antd';
 import logo from './logo.svg';
 import './App.css';
@@ -7,14 +8,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button type="primary"> Hello How are you </Button>
+        <Button type="primary"> Hello How are you </Button> */}
+        <Container />
       </div>
     );
   }
